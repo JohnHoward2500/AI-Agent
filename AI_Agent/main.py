a444d72129b0b68a33d513b1bf3cc3b7b9a2d488
John Howard <howardjohn2500@gmail.com>
2025-08-07 01:27:02 -0400

    added calculator

--- AI_Agent/main.py
+++ AI_Agent/main.py
@@ -3,7 +3,11 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
 
+# Gets input info
 user_input = sys.argv[1:][0]
 verbose = None
 if len(sys.argv) >= 3:
@@ -12,14 +16,14 @@ messages = [
     types.Content(role="user", parts=[types.Part(text=user_input)])
 ]
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+# Sends input info through gemini
 response = client.models.generate_content(
     model = 'gemini-2.0-flash-001',
     contents = messages
 )
-if verbose:
+
+# Output
+if verbose.lower() == "--verbose":
     print(f"User prompt: {user_input}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
