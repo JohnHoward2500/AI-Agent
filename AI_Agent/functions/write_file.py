b4dabd3b99c4532137d1925ee5e5b76600325be7
John Howard <howardjohn2500@gmail.com>
2025-08-09 05:39:55 -0400

    optimized write_file.py

--- AI_Agent/functions/write_file.py
+++ AI_Agent/functions/write_file.py
@@ -7,11 +7,16 @@ def write_file(working_directory, file_path, content):
     if not file_path_abs.startswith(working_dir_abs):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
     
-    try:
-        if not os.path.exists(os.path.abspath(os.path.dirname(file_path))):
+    
+    if not os.path.exists(file_path_abs):
+        try:
             os.makedirs(os.path.abspath(os.path.dirname(file_path)))
+        except Exception as e:
+            return f'Error: "{file_path}" is a directory, not a file'
+
+    try:
         with open(file_path_abs, "w") as f:
             f.write(content)
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
-        return f'Error writing file: {e}'
+        return f'Error writing to file: {e}'
